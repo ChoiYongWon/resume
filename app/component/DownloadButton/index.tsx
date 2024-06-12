fc40924e9e2f4586d982600437f891e3e06174ae
ChoiYongWon <yongwon0824@naver.com>
2024-06-12 17:30:09 +0900

    Introduce에 보충역 추가

--- app/component/DownloadButton/index.tsx
+++ app/component/DownloadButton/index.tsx
@@ -5,7 +5,7 @@ import DownloadButtonImage from "../../../public/svg/download.svg"
 
 const DownloadButton = () => {
     return (
-        <Link href={"/file/최용원 이력서 FE (산업기능요원).pdf"} download={"최용원 이력서 FE (산업기능요원).pdf"} className={DownloadButtonStyle}>
+        <Link href={"/file/최용원 이력서 FE (산업기능요원 보충역).pdf"} download={"최용원 이력서 FE (산업기능요원 보충역).pdf"} className={DownloadButtonStyle}>
             <Image src={DownloadButtonImage} alt="" width={18} height={18}/>
         </Link>
     )
