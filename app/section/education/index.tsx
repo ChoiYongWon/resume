97e4fca707dccf48f1b60d91e53989d48e94124f
ChoiYongWon <yongwon0824@naver.com>
2024-06-24 17:02:35 +0900

    Education 학교 순서 수정

--- app/section/education/index.tsx
+++ app/section/education/index.tsx
@@ -25,10 +25,6 @@ const Education = ({ style }: Props) => {
             title="디지털 인재 양성과정" status="수료" day="2023. 02. 07. ~ 2023. 03. 03." host="메가존클라우드 아이티 평생교육원">
               <EducationCard.Content>네트워크 및 AWS 클라우드 기술을 학습했으며, 우연한 발견 프로젝트를 진행하는 과정에서 AWS ECS 기반의 MSA 구축에 활용</EducationCard.Content>
         </EducationCard>
-        <EducationCard
-            style={{marginTop: "30px"}}
-            title="계명대학교 컴퓨터공학과 학사" status="졸업 예정" day="2020. 03. ~ 2024. 8." host="계명대학교">
-        </EducationCard>
     </div>
   );
 };
