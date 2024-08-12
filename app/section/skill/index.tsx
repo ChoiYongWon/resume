56abe178c1890e098db033f54484024e03b3b70f
ChoiYongWon <yongwon0824@naver.com>
2024-08-13 04:34:43 +0900

    Skill 상세화

--- app/section/skill/index.tsx
+++ app/section/skill/index.tsx
@@ -13,10 +13,9 @@ const Skill = ({ style }: Props) => {
         <Topic title="Frontend" style={{marginTop: "20px"}}>
           <ul className={UlStyle}>
             <li className={LiStyle}>Next.js, React, TypeScript 환경에 익숙합니다.</li>
-            <li className={LiStyle}>React 생명주기를 이해하고 적절히 활용할 수 있습니다.</li>
-            <li className={LiStyle}>React의 동작 과정을 얕게 이해하고 있습니다.</li>
-            <li className={LiStyle}>SSR의 원리를 이해하고 있으며 활용할 수 있습니다.</li>
-            <li className={LiStyle}>브라우저가 렌더링하는 과정을 이해하고 있습니다.</li>
+            <li className={LiStyle}>React의  Fiber 아키텍처를 이해하고 있으며, Render 및 Commit Phase의 세부 동작과정을 얕게 이해하고 있습니다.</li>
+            <li className={LiStyle}>Next.js의 SSR 프로세스를 이해하고 있습니다. RSC Payload의 생성 과정, Hydration 과정을 설명할 수 있습니다.</li>
+            <li className={LiStyle}>Critical Rendering Path의 주요 단계를 이해하고 웹 최적화에 적용할 수 있습니다.</li>
             <li className={LiStyle}>자바스크립트 동작 과정을 이해하고 있습니다.</li>
             {/* <li className={LiStyle}>시맨틱 태그를 준수하는 것을 선호합니다.</li> */}
           </ul>
@@ -44,7 +43,7 @@ const Skill = ({ style }: Props) => {
         {/* 피드백 반영 (스킬 상세화) */}
         {/* <Topic title="Frontend" style={{marginTop: "20px"}}>
             <Topic.Skills list={["html5", "css3", "javascript(es6)", "typescript"]}/>
-            <Topic.Skills list={["react", "next.js"]}/>
+            <Topic.Skills list={["reactjs", "nextjs"]}/>
         </Topic>
         <Topic title="Backend" style={{marginTop: "30px"}}>
             <Topic.Skills list={["nestjs"]}/>
